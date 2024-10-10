8a9d7b585f01779fcfde86032349a3e635dc0890
Downupanddownup <pingdengjia0liu@163.com>
2024-10-10 18:39:15 +0800

    添加web基础结构

--- server/bean/inference_task/vits_model.py
+++ server/bean/inference_task/vits_model.py
@@ -1,4 +1,7 @@
+import os
+
 from server.bean.base_model import BaseModel
+from server.dao.data_base_manager import db_config
 
 
 class VitsModel(BaseModel):
@@ -9,3 +12,16 @@ class VitsModel(BaseModel):
 
     def equals(self, version, name):
         return self.version == version and self.name == name
+
+    @staticmethod
+    def create_dir():
+        os.makedirs(VitsModel.get_base_v1_dir(), exist_ok=True)
+        os.makedirs(VitsModel.get_base_v2_dir(), exist_ok=True)
+
+    @staticmethod
+    def get_base_v1_dir():
+        return f'{db_config.get_work_dir()}/SoVITS_weights'
+
+    @staticmethod
+    def get_base_v2_dir():
+        return f'{db_config.get_work_dir()}/SoVITS_weights_v2'
